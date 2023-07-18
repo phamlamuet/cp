84a982c30c38fcad90d44a6b81ec8053838cf372
Lam Pham <phamlamuet@gmail.com>
2023-07-19 01:31:30 +0700

    overflow!!!, using long all the time!!!

---
+++ src/main/java/Problem4.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Problem4 {
+    int n;
+    int[] arr;
+
+    long solve() {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+
+        if (n < 2) {
+            return 0;
+        }
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        long totalNeedToAdd = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            int needToAdd = arr[i-1] - arr[i];
+            if (needToAdd <= 0) {
+                continue;
+            } else {
+                totalNeedToAdd += needToAdd;
+                //update current element
+                arr[i] += needToAdd;
+            }
+        }
+
+        return totalNeedToAdd;
+    }
+
+    public static void main(String[] args) {
+        Problem4 problem4 = new Problem4();
+        System.out.println(problem4.solve());
+
+    }
+}
